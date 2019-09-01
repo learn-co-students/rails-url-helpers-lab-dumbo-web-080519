d6acd30adc07f3f5ac328948f376d1c2de4af6df
github username <github email address>
2019-09-01 14:09:57 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
 class Student < ActiveRecord::Base
+  attr_reader :active
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def active_status
+    if self.active == nil
+      false
+    else
+      true
+    end
   end
 end
